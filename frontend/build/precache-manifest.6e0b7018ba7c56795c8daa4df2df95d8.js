063f63b3863fad680d8e1ad5ce0a1fd1cb64c063
kststudio <kststudio@gmail.com>
2019-11-17 21:23:23 -0500

    creates build directory

---
+++ frontend/build/precache-manifest.6e0b7018ba7c56795c8daa4df2df95d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7f8aa3272d95376960c9b607f200071",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a4c9ddf5fe8aa42cc1a6",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "d330047bbc998ba081eb",
+    "url": "/static/js/2.46fba826.chunk.js"
+  },
+  {
+    "revision": "a4c9ddf5fe8aa42cc1a6",
+    "url": "/static/js/main.12d46bbf.chunk.js"
+  },
+  {
+    "revision": "9aabb23bcbd2c9e69d36",
+    "url": "/static/js/runtime-main.baeb1881.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
